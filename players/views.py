b470506e836e3f019013afa3847c1efdf86b7dd6
J. Heasly <jheasly@earthlink.net>
2020-06-22 22:32:16 -0700

    basics down

--- players/views.py
+++ players/views.py
@@ -5,6 +5,14 @@ from .models import PlayerTeamStint
 # Create your views here.
 class StatsSeasonsTeamsListView(ListView):
     def get_queryset(self):
-        return PlayerTeamStint.objects.filter(date_joined__contains='2020')
-        # return PlayerTeamStint.objects.select_related('team').select_related('player').filter(date_joined__contains='2020')
+        # returned in 50ms, 41 queries
+        # return PlayerTeamStint.objects.filter(date_joined__contains='2020')
 
+        # select_related magic: returned in 10ms, 1 query
+        return PlayerTeamStint.objects.select_related('team').select_related('player').filter(date_joined__contains='2020')
+
+    def get_context_data(self, **kwargs):
+        '''Hard wire the season in a kinda elegant way'''
+        context = super().get_context_data(**kwargs)
+        context['season'] = '2020'
+        return context
